0063f851049bb8d4f8fcdd9a998070729d1599fe
AshJayy <ashwinie.ja@gmail.com>
2024-04-20 20:08:02 +0530

    Added custom color psllet

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,20 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'purple': {
+        DEFAULT: '#872b97'
+      },
+      'pink': {
+        DEFAULT: '#ff3c68'
+      },
+      'orange': {
+        DEFAULT: '#ff7130'
+      },
+      'darkgray': {
+        DEFAULT: '#171721'
+      }
+    }
   },
   plugins: [
     flowbite.plugin(),
