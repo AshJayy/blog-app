5c6cfbf9be9d3d023727d45a397b157075164158
AshJayy <ashwinie.ja@gmail.com>
2024-05-05 11:19:56 +0530

    <Dashboard> Sidebar and Profile basic set up

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSideBar from "../components/DashSideBar";
+import DashProfile from "../components/DashProfile";
+
 export default function Dashboard() {
+
+  const location = useLocation()
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  }, [location.search])
+
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col sm:flex-row">
+      <div>
+        {/* Sidebar */}
+        <DashSideBar />
+      </div>
+      <div>
+        {/* Profile */}
+        {tab === 'profile' && <DashProfile />}
+      </div>
     </div>
   )
 }
