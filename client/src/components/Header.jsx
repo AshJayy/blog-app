d50bd2504af4971ac8957c53f024a6eb41d3ada8
AshJayy <ashwinie.ja@gmail.com>
2024-05-07 22:29:42 +0530

    <Header> Added sign out functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon, FaSun} from 'react-icons/fa'
 import { useDispatch } from "react-redux"
+import { signOutSuccess } from "../redux/User/userSlice";
 import { toggleTheme } from "../redux/theme/theme.slice.js"
 import { useSelector } from 'react-redux'
 import Logo from './Logo.jsx'
@@ -27,8 +28,28 @@ export default function Header() {
   ]
   const path = useLocation().pathname;
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const {currentUser} = useSelector(state => state.user);
   const {theme} = useSelector(state => state.theme);
+
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch('api/user/signout', {
+        method: 'POST'
+      });
+      const data = await res.json();
+
+      if(!res.ok){
+        console.log(data.message);
+      }else{
+        dispatch(signOutSuccess());
+        navigate('/sign-in');
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <Navbar className="border-b-2">
@@ -83,7 +104,7 @@ export default function Header() {
             <Link to={'/dashboard?tab=profile'}>
               <Dropdown.Item>Profile</Dropdown.Item>
               <Dropdown.Divider />
-              <Dropdown.Item>Sign Out</Dropdown.Item>
+              <Dropdown.Item onClick={handleSignOut}>Sign Out</Dropdown.Item>
             </Link>
           </Dropdown>
           ) : (
