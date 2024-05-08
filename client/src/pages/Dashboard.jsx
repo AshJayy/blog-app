c7906b6590d0a17c235dcd28e6c9204744170e57
AshJayy <ashwinie.ja@gmail.com>
2024-05-08 21:34:48 +0530

    <Dashboard> Added post page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSideBar from "../components/DashSideBar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 export default function Dashboard() {
 
@@ -24,6 +25,8 @@ export default function Dashboard() {
       <div className="w-full">
         {/* Profile */}
         {tab === 'profile' && <DashProfile />}
+        {/* Posts */}
+        {tab === 'posts' && <DashPosts />}
       </div>
     </div>
   )
